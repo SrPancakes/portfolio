80455c1e93496811be5eb998192a48b9a348ae9f
SrPancakes <adormilucasnunez@gmail.com>
2024-01-30 10:44:26 +0100

    Several changes in zustand store

--- src/components/store/projectStore.js
+++ src/components/store/projectStore.js
@@ -2,9 +2,19 @@ import { create } from 'zustand';
 import PROJECTS from "../../data/projects.json";
 import ARTICLES from "../../data/articles.json";
 
-export const useProjectStore = create((set) => ({
+const useProjectStore = create((set) => ({
   currentProject: PROJECTS[0],
   setCurrentProject: (currentProject) => set({ currentProject }),
   currentArticle: ARTICLES[0],
   setCurrentArticle: (currentArticle) => set({ currentArticle }),
 }))
+
+const { currentProject, setCurrentProject, currentArticle, setCurrentArticle } = useProjectStore
+
+export {
+  useProjectStore,
+  currentProject,
+  setCurrentProject,
+  currentArticle,
+  setCurrentArticle
+}
